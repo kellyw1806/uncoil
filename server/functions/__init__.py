318fa5f81ec23f8794995a982b9f9ee70a5f0ff6
Steven Wu <wusteven815@gmail.com>
2025-01-12 00:22:54 -0500

    feat: interface

---
+++ server/functions/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+
+def create_exercises(data: dict) -> list[str]:
+    return ["Pushups", "Situps", "Squats"]
+
+def create_plan(exercises: list[str]) -> str:
+    return "Many exercises!"
+
+def parse_injury(text: str) -> str:
+    return "knee"
